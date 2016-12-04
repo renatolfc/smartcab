bd859606ff7425b7290f23885dcc8e86e3e37fba
Renato L. F. Cunha <renatocunha@acm.org>
2016-12-04 09:14:20 -0200

    agent: implement random agent and add state information

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -5,7 +5,7 @@ from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
 
-UPDATE_STRING = 'LearningAgent.update(): deadline = {}, inputs = {},' + \
+UPDATE_STRING = 'LearningAgent.update(): deadline = {}, inputs = {}, ' + \
                 'action = {}, reward = {}'
 
 
@@ -62,7 +62,7 @@ class HardCodedAgent(Agent):
         print(UPDATE_STRING).format(deadline, inputs, action, reward)
 
 
-class LearningAgent(Agent):
+class RandomAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
     def __init__(self, env):
@@ -80,16 +80,65 @@ class LearningAgent(Agent):
 
     def update(self, t):
         # Gather inputs
-
         # from route planner, also displayed by simulator
         self.next_waypoint = self.planner.next_waypoint()
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
+        # do nothing for now
 
-        # TODO: Select action according to your policy
-        action = None
+        # For now we only generate random actions
+        action = random.choice(Environment.valid_actions[1:])
+
+        # Execute action and get reward
+        reward = self.env.act(self, action)
+
+        # TODO: Learn policy based on state, action, reward
+
+        print(UPDATE_STRING).format(deadline, inputs, action, reward)
+
+
+class LearningAgent(Agent):
+    """An agent that learns to drive in the smartcab world."""
+
+    def __init__(self, env):
+        # sets self.env = env, state = None, next_waypoint = None
+        super(LearningAgent, self).__init__(env)
+        self.color = 'white'  # override color
+        # simple route planner to get next_waypoint
+        self.planner = RoutePlanner(self.env, self)
+        # TODO: Initialize any additional variables here
+        self.next_waypoint = None
+
+    def reset(self, destination=None):
+        self.planner.route_to(destination)
+        self.destination = destination
+        self.state = [0] * 5
+        # TODO: Prepare for a new trip; reset any variables here, if required
+
+    @staticmethod
+    def _get_action_index(action):
+        return Environment.valid_actions.index(action)
+
+    def update(self, t):
+        # Gather inputs
+        # from route planner, also displayed by simulator
+        self.next_waypoint = self.planner.next_waypoint()
+        inputs = self.env.sense(self)
+        deadline = self.env.get_deadline(self)
+
+        # deadline = 18, inputs = {'light': 'red', 'oncoming': None, 'right':
+        # None, 'left': None},action = right, reward = 1
+        self.state = [
+            deadline, deadline < 0, inputs['light'] == 'red',
+            self._get_action_index(inputs['oncoming']),
+            self._get_action_index(inputs['left']),
+            self._get_action_index(inputs['right']),
+        ]
+
+        # For now we only generate random actions
+        action = random.choice(Environment.valid_actions[1:])
 
         # Execute action and get reward
         reward = self.env.act(self, action)
